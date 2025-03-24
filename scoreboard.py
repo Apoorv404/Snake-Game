a48750ce12376e86c2ea83796cd4faeb269c01ec
Apoorv404 <jadhavapoorv2244@gmail.com>
2025-03-24 21:57:40 +0530

    added the functionality of high score

--- scoreboard.py
+++ scoreboard.py
@@ -12,16 +12,26 @@ class Scoreboard(Turtle):
         self.goto(0,270)
         self.color("white")
         self.score = 0
-        self.display()
+        with open("data.txt", mode="r") as data:
+            self.high_score = int(data.read())
+        self.update_score()
 
-    def display(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.home()
+    #     self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.home()
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
 
-    def update(self):
+    def update_score(self):
         self.clear()
+        self.write(arg=f"Score: {self.score}   High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
         self.score += 1
-        self.display()
+        self.update_score()
